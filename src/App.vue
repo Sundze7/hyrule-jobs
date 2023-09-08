1f5cc1941302748564f985674409a2a6c83ec1c0
Sundze <keyonmwesundze@mail.com>
2023-09-08 08:16:32 +0100

    ts concepts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
-  <div class="app">hello there</div>
+  <div class="app">
+    <JobList :jobs="jobs" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import "./assets/global.css";
+import Job from "./types/Job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 300000,
+        id: "1",
+      },
+      {
+        title: "fisher",
+        location: "seaport",
+        salary: 30000,
+        id: "2",
+      },
+      {
+        title: "worker",
+        location: "tranch",
+        salary: 300000,
+        id: "3",
+      },
+    ]);
+
+    return { jobs };
+  },
 });
 </script>
-
-<style></style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="job-list">
+    <li v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }} in {{ job.location }}</h2>
+      <div class="salary">
+        <p>{{ job.salary }} FCFA</p>
+      </div>
+      <div class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis provident
+        enim incidunt repellendus illo, neque rerum pariatur iure aut deserunt
+        ducimus tempora, fuga eius nesciunt?
+      </div>
+    </li>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from "vue";
+import Job from "@/types/Job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+  },
+});
+</script>
